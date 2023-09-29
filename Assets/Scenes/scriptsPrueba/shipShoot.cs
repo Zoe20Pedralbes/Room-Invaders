21b229cdbdd1edde90fad6386219ac5f564d9b99
Zoe <a23zoeguepar@inspedralbes.cat>
2023-09-29 17:16:17 +0200

    Shot working
    El disparo funciona bien, aun no esta implementada su colision.

---
+++ Assets/Scenes/scriptsPrueba/shipShoot.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class shipShoot : MonoBehaviour
+{
+    public List<Transform> spawnPoints = new List<Transform>();
+    public GameObject bulletPrefab;
+    public InputActionAsset shootingAction;
+    [SerializeField] private int indexSpawn = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootingAction.FindActionMap("shoot").FindAction("Misil").performed += launchMissil;
+    }
+
+
+    void launchMissil(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Shot");
+        indexSpawn = (indexSpawn + 1) % spawnPoints.Count;
+        //GameObject lastBullet = Instantiate(bulletPrefab, spawnPoints[indexSpawn].position, Quaternion.identity);
+        GameObject lastBullet = Instantiate(bulletPrefab, spawnPoints[indexSpawn].position, Quaternion.identity);
+        lastBullet.GetComponent<Bullet>().SetDirection(transform.forward);
+        //float shipSpeed = GetComponent<shipMovement>();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (Transform t in spawnPoints)
+        {
+            Gizmos.DrawWireSphere(t.position, 0.25f);
+            Gizmos.DrawSphere(t.position, .10f);
+        }
+    }
+
+    private void OnEnable()
+    {
+        shootingAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        shootingAction.Enable();
+
+    }
+}
